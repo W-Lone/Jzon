2d0ad89b9f8ecddc3e388e6e13e2c5e75f80bf57
W-Lone <aime.saysset@gmail.com>
2023-03-27 08:59:50 +0200

    CouCou

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("Uwu")
+// fetch('data.php') 
+//     .then(response=>response.json())
+//     .then(data=>console.log(data))
+fetch('https://yesno.wtf/api') 
+    .then(response=>response.json())
+    .then(data=>console.log(data))
---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-Type:application/json");
+$data=[
+    "name"=>"hugo",
+    "note"=>6
+];
+echo json_encode($data);
+?>
